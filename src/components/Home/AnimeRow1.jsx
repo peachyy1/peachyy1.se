dcc1b4630289d880a5e1905c5b20ab89a2779696
unknown <jonte55@live.se>
2024-06-27 22:30:02 +0200

    added links to all image- and text-cards

--- src/components/Home/AnimeRow1.jsx
+++ src/components/Home/AnimeRow1.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import r1C1 from "../../assets/images/anime/anime-posters/r1-c1.jpg";
 import r1C2 from "../../assets/images/anime/anime-posters/r1-c2.jpg";
 import r1C3 from "../../assets/images/anime/anime-posters/r1-c3.jpg";
@@ -9,45 +10,59 @@ const AnimeRow1 = () => {
             <div className="anime-row-1">
                 <div className="anime-row-1-col-1">
                     <div className="anime-card">
-                        <img src={r1C1} alt="" />
+                        <Link to="/Evangelion">
+                            <img src={r1C1} alt="evangelion" />
+                        </Link>
                     </div>
                     <div className="anime-text">
-                        <p>
-                            A teenage boy finds himself recruited as a member of an elite team of pilots by his father.
-                        </p>
+                        <Link to="/Evangelion">
+                            <p>
+                                A teenage boy finds himself recruited as a member of an elite team of pilots by his father.
+                            </p>
+                        </Link>
                     </div>
                 </div>
                 <div className="anime-row-1-col-2">
                     <div className="anime-card">
-                        <img src={r1C2} alt="" />
-
+                        <Link to="/End-of-Evangelion">
+                            <img src={r1C2} alt="" />
+                        </Link>
                     </div>
                     <div className="anime-text">
-                        <p>
-                            Concurrent theatrical ending of the TV series Shin seiki evangerion (1995).
-                        </p>
+                        <Link to="/End-of-Evangelion">
+                            <p>
+                                Concurrent theatrical ending of the TV series Shin seiki evangerion (1995).
+                            </p>
+                        </Link>
                     </div>
                 </div>
                 <div className="anime-row-1-col-3">
                     <div className="anime-card">
-                        <img src={r1C3} alt="" />
-
+                        <Link to="/A-Silent-Voice">
+                            <img src={r1C3} alt="" />
+                        </Link>
                     </div>
                     <div className="anime-text">
-                        <p>
-                            A young man is ostracized by his classmates after he bullies a deaf girl to the point where she moves away. Years later, he sets off on a path for redemption.
-                        </p>
+                        <Link to="/A-Silent-Voice">
+                            <p>
+                                A young man is ostracized by his classmates after he bullies a deaf girl to the point where she moves away. Years later, he sets off on a path for redemption.
+                            </p>
+                        </Link>
                     </div>
                 </div>
                 <div className="anime-row-1-col-4">
                     <div className="anime-card">
-                        <img src={r1C4} alt="" />
+                        <Link to="/Kill-la-Kill">
+                            <img src={r1C4} alt="" />
+                        </Link>
 
                     </div>
                     <div className="anime-text">
-                        <p>
-                            A young girl arrives at a school of superhumans to find out the truth behind her father's murder.
-                        </p>
+                        <Link to="/Kill-la-Kill">
+                            <p>
+                                A young girl arrives at a school of superhumans to find out the truth behind her father's murder.
+                            </p>
+                        </Link>
                     </div>
                 </div>
             </div>
